9fc5bd1ff8cf2dc5dee5651ae8bc52701669d13e
haron47 <gho96040730437!>
2021-12-14 03:53:11 +0900

    chore : 오타 수정

--- src/main/java/Study/SpringStudy/service/MemberService.java
+++ src/main/java/Study/SpringStudy/service/MemberService.java
@@ -12,13 +12,13 @@ public class MemberService {
 
     // 회원가입
     public Long join(Member member){
-        validateDupicateMember(member);
+        validateDuplicateMember(member);
 
         memberRepository.save(member);
         return member.getId();
     }
 
-    private void validateDupicateMember(Member member) {
+    private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m-> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
