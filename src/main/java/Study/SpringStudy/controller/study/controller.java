5c1e94f5b86db788432d1eff8495bab34ac22269
haron47 <gho96040730437!>
2021-12-13 21:47:43 +0900

    feat : view 환경설정

---
+++ src/main/java/Study/SpringStudy/controller/study/controller.java
@@ -0,0 +1,15 @@
+package Study.SpringStudy.controller.study;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping("study")
+    public String study(Model model){
+        model.addAttribute("data", "Study now!");
+        return "study";
+    }
+}
