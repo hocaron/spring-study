81110fda8f19f77ae4ccc8781ee4889a766ac0fe
haron47 <gho96040730437!>
2021-12-14 03:20:27 +0900

    test : 회원 리포티토리 테스트 케이스 작성

--- src/main/java/Study/SpringStudy/repository/MemoryMemberRepository.java
+++ src/main/java/Study/SpringStudy/repository/MemoryMemberRepository.java
@@ -23,12 +23,16 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().equals(name)).
-                findAny();
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
