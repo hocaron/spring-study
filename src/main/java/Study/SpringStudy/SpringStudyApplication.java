a38c758d30b86d744562e9ca043b69932d45fd56
unknown <pooh0407@ewhain.net>
2021-12-13 21:19:16 +0900

    feat : init 설정

---
+++ src/main/java/Study/SpringStudy/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package Study.SpringStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Study/SpringStudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.SpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
