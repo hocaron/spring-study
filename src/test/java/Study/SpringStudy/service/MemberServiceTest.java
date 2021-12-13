8c3ed468c51bded5e33ab592faf8aefb1f8184d4
haron47 <gho96040730437!>
2021-12-14 07:17:41 +0900

    test : 회원 서비스 테스트 케이스 추가 작성

--- src/test/java/Study/SpringStudy/service/MemberServiceTest.java
+++ src/test/java/Study/SpringStudy/service/MemberServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,7 +45,7 @@ class MemberServiceTest {
     public void validateDuplicateMember(){
         Member member1 = new Member();
         member1.setName("spring1");
-        Long savedId = memberService.join(member1);
+        memberService.join(member1);
 
         Member member2 = new Member();
         member2.setName("spring1");
@@ -53,9 +55,29 @@ class MemberServiceTest {
 
     @Test
     void findMembers() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberService.join(member2);
+
+        List<Member> result = memoryMemberRepository.findAll();
+        assertThat(result).isEqualTo(memberService.findMembers());
     }
 
     @Test
     void findOne() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        Long savedId = memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberService.join(member2);
+
+        Member result = memberService.findOne(savedId).get();
+        assertThat(result).isEqualTo(member1);
     }
 }
