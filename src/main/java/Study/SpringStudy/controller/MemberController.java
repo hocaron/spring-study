148b4ce389cbaa1ff932d76239906ac9f65d2708
haron47 <pooh0407@ewhain.net>
2021-12-20 04:59:54 +0900

    feat : 회원 조회 로직 구현

--- src/main/java/Study/SpringStudy/controller/MemberController.java
+++ src/main/java/Study/SpringStudy/controller/MemberController.java
@@ -4,10 +4,12 @@ import Study.SpringStudy.domain.Member;
 import Study.SpringStudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.Constructor;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +33,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String getMembers(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
