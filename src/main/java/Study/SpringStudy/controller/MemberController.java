af4dbc7e65bf599d3a2a25cbbd3db23523a6e12c
haron47 <pooh0407@ewhain.net>
2021-12-20 04:52:59 +0900

    feat :: 회원 등록 로직 구현

--- src/main/java/Study/SpringStudy/controller/MemberController.java
+++ src/main/java/Study/SpringStudy/controller/MemberController.java
@@ -1,8 +1,11 @@
 package Study.SpringStudy.controller;
 
+import Study.SpringStudy.domain.Member;
 import Study.SpringStudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.lang.reflect.Constructor;
 
@@ -13,5 +16,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("members/new")
+    public String createForm(){
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/Study/SpringStudy/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package Study.SpringStudy.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
