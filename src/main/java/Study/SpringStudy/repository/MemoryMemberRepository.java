37666b79964ee97a39abf110c8cc3e6d624d68a3
haron47 <pooh0407@ewhain.net>
2021-12-20 04:22:43 +0900

    feat :: 직접 스프링빈 등록

--- src/main/java/Study/SpringStudy/repository/MemoryMemberRepository.java
+++ src/main/java/Study/SpringStudy/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
--- src/main/java/Study/SpringStudy/service/MemberService.java
+++ src/main/java/Study/SpringStudy/service/MemberService.java
@@ -8,11 +8,9 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
