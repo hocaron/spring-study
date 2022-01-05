6a2de01db05ededbee48bb244277b69adbd88b75
haron47 <pooh0407@ewhain.net>
2022-01-05 20:05:07 +0900

    feat: create member repository

---
+++ src/main/java/backend/spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package backend.spring.repository;
+
+import backend.spring.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
