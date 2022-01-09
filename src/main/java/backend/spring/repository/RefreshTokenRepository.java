01bee45889ad65d01a21f4c880b196242fee902e
haron47 <pooh0407@ewhain.net>
2022-01-09 16:20:53 +0900

    feat: create refresh token entity / repository

---
+++ src/main/java/backend/spring/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package backend.spring.repository;
+
+import backend.spring.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface  RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
