3f21601f0d08e3a1fd1db10210a65723ebdf947c
haron47 <pooh0407@ewhain.net>
2022-01-11 17:45:12 +0900

    feat: spring swagger 프로젝트 생성

---
+++ spring-swagger/src/main/java/com/example/springswagger/SpringswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringswaggerApplication.class, args);
+	}
+
+}
