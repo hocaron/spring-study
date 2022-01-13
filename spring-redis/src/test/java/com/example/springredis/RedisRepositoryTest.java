8412fe7f7c869f5796e2bd072474fbcba70c623e
haron47 <pooh0407@ewhain.net>
2022-01-13 16:49:46 +0900

    feat: redis repository 생성

---
+++ spring-redis/src/test/java/com/example/springredis/RedisRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.springredis;
+
+import com.example.springredis.entity.Person;
+import com.example.springredis.repository.PersonRedisRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisRepositoryTest {
+
+    @Autowired
+    private PersonRedisRepository repo;
+
+    @Test
+    void test() {
+        Person person = new Person("Ho", 20);
+
+        // 저장
+        repo.save(person);
+
+        // `keyspace:id` 값을 가져옴
+        repo.findById(person.getId());
+
+        // Person Entity 의 @RedisHash 에 정의되어 있는 keyspace (people) 에 속한 키의 갯수를 구함
+        repo.count();
+
+        // 삭제
+        repo.delete(person);
+    }
+}
