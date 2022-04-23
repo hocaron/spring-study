f5c559ff9928616c975db8d47049eccb3bd0ab8b
hocaron <pooh0407@ewhain.net>
2022-04-23 18:30:04 +0900

    init : 로깅을 위한 프로젝트 생성

---
+++ spring-logging/src/test/java/com/springstudy/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
