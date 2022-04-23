edcc8fcc094939e6f4ff064fa02de30dff714714
hocaron <pooh0407@ewhain.net>
2022-04-23 19:07:42 +0900

    feat : 레벨별 에러 작성

--- spring-logging/src/main/java/com/springstudy/logging/LoggingApplication.java
+++ spring-logging/src/main/java/com/springstudy/logging/LoggingApplication.java
@@ -1,13 +1,21 @@
 package com.springstudy.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoggingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(LoggingApplication.class);
+        for (int count = 1; count <= 10; count++) {
+            logger.trace("trace 로깅이야!!! {}", count);
+            logger.debug("debug 로깅이야!!! {}", count);
+            logger.info("info 로깅이야!!! {}", count);
+            logger.warn("warn 로깅이야!!! {}", count);
+            logger.error("error 로깅이야!!! {}", count);
+        }
+    }
 }
