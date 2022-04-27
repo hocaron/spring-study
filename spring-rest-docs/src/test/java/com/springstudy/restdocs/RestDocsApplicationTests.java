7acc74f3f9d73c00bdf8567ff654d878c387ea7c
hocaron <pooh0407@ewhain.net>
2022-04-28 07:27:21 +0900

    init : rest docs 실습을 위한 프로젝트 생성

---
+++ spring-rest-docs/src/test/java/com/springstudy/restdocs/RestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.restdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
