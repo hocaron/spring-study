3d8570895252e6aad7d71c554e9e5015b1364ab2
hocaron <pooh0407@ewhain.net>
2022-06-03 11:42:46 +0900

    chore : sql 중복 스크립트 삭제

--- spring-graphql/src/main/resources/data.sql
+++ spring-graphql/src/main/resources/data.sql
@@ -8,13 +8,3 @@ VALUES (1, 1, 'ROLE_ADMIN'),
        (2, 1, 'ROLE_MEMBER'),
        (3, 2, 'ROLE_MEMBER'),
        (4, 3, 'ROLE_MANAGER');
-INSERT INTO member (id, login_id, password, name)
-VALUES (1, 'member', 'password', '멤버'),
-       (2, 'account', 'password', '계정'),
-       (3, 'user', 'password', '사용자');
-
-INSERT INTO role (id, member_id, role)
-VALUES (1, 1, 'ROLE_ADMIN'),
-       (2, 1, 'ROLE_MEMBER'),
-       (3, 2, 'ROLE_MEMBER'),
-       (4, 3, 'ROLE_MANAGER');
