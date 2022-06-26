18ca71d450f39e8738e50c689f3ca6826d7a977d
hocaron <kkannu0407@gmail.com>
2022-06-26 18:40:07 +0900

    feat : 조회 메서드 추가

--- spring-jpa/src/main/java/com/springstudy/jpa/member/MemberRepository.java
+++ spring-jpa/src/main/java/com/springstudy/jpa/member/MemberRepository.java
@@ -3,5 +3,5 @@ package com.springstudy.jpa.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Member findByNickname(String nickname);
 }
