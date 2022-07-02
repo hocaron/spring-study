27edf6cb82af10d8d7506b3317bb9055cddd1343
hocaron <kkannu0407@gmail.com>
2022-07-03 00:12:43 +0900

    feat: api-gateway 설정

--- spring-msa/gateway/src/main/java/com/springstudy/gateway/GatewayApplication.java
+++ spring-msa/gateway/src/main/java/com/springstudy/gateway/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.springstudy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
