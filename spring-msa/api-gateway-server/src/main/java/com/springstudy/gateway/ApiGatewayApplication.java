77ccb6d087f9933c0631d4fd5a41c4a35c5f45c7
hocaron <kkannu0407@gmail.com>
2022-07-03 01:53:20 +0900

    chore: 프로젝트 이름 변경

--- spring-msa/api-gateway-server/src/main/java/com/springstudy/gateway/ApiGatewayApplication.java
+++ spring-msa/api-gateway-server/src/main/java/com/springstudy/gateway/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class GatewayApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
