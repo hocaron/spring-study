84afbd2edeee5751b46a087ad326e7a6111a8639
hocaron <kkannu0407@gmail.com>
2022-07-04 01:35:24 +0900

    build: jib 설정 추가

--- spring-msa/member-service/src/main/java/com/springstudy/jpa/MemberApplication.java
+++ spring-msa/member-service/src/main/java/com/springstudy/jpa/MemberApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaApplication {
+public class MemberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpaApplication.class, args);
+		SpringApplication.run(MemberApplication.class, args);
 	}
 
 }
