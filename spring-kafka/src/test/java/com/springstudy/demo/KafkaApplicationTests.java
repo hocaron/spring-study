6edbf07c715d7e87d637498cd47ebb93915e2a0d
hocaron <kkannu0407@gmail.com>
2022-08-16 01:06:23 +0900

    init: 카프카 프로젝트 생성

---
+++ spring-kafka/src/test/java/com/springstudy/demo/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
