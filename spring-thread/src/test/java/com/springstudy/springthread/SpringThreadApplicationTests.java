73fe74abf7beb658372aa43b45605eddca1a6779
hocaron <kkannu0407@gmail.com>
2022-10-01 23:49:37 +0900

    init: 쓰레드 테스트를 위한 프로젝트 생성

---
+++ spring-thread/src/test/java/com/springstudy/springthread/SpringThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.springthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
