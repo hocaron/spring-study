c56295ac3bd40882039ea1118d5e9bed0404df2e
hocaron <kkannu0407@gmail.com>
2023-02-26 22:40:14 +0900

    feat: @component 와 @configuratin 차이 테스트

---
+++ spring-annotation/src/main/java/com/springstudy/annotation/componentVSconfiguration/SimpleBean.java
@@ -0,0 +1,7 @@
+package com.springstudy.annotation.componentVSconfiguration;
+
+public class SimpleBean {
+	public SimpleBean() {
+		System.out.println("SimpleBean is being created");
+	}
+}
---
+++ spring-annotation/src/main/java/com/springstudy/annotation/componentVSconfiguration/SimpleBeanConsumer.java
@@ -0,0 +1,9 @@
+package com.springstudy.annotation.componentVSconfiguration;
+
+public class SimpleBeanConsumer {
+	private SimpleBean simpleBean;
+
+	public SimpleBeanConsumer(SimpleBean simpleBean) {
+		this.simpleBean = simpleBean;
+	}
+}
