da8a5498fac0fdd9a1044bd45dccb39e585f5e0f
hocaron <kkannu0407@gmail.com>
2023-09-20 00:54:02 +0900

    init: dto 테스트 프로젝트 생성

---
+++ spring-dto/src/main/java/com/springstudy/dto/DtoApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoApplication.class, args);
+	}
+
+}
---
+++ spring-dto/src/test/java/com/springstudy/dto/DtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
