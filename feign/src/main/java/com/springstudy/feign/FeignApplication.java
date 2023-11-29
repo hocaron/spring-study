3d6167a4db7ddd993cd5ea616ae62d5e7280ca24
hocaron <kkannu0407@gmail.com>
2023-11-30 00:51:04 +0900

    init: feign http client 테스트 프로젝트 생성

---
+++ feign/src/main/java/com/springstudy/feign/FeignApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+
+}
