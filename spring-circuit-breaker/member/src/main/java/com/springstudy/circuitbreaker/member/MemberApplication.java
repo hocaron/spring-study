9d0620ad519dd30d5efa61360be3b39f8391b9ea
hocaron <kkannu0407@gmail.com>
2023-12-24 23:25:49 +0900

    feat: 서킷 브레이커 및 액튜에이터 설정 추가

--- spring-circuit-breaker/member/src/main/java/com/springstudy/circuitbreaker/member/MemberApplication.java
+++ spring-circuit-breaker/member/src/main/java/com/springstudy/circuitbreaker/member/MemberApplication.java
@@ -2,6 +2,7 @@ package com.springstudy.circuitbreaker.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class MemberApplication {
--- spring-circuit-breaker/member/src/main/java/com/springstudy/circuitbreaker/member/client/PointFeignClient.java
+++ spring-circuit-breaker/member/src/main/java/com/springstudy/circuitbreaker/member/client/PointFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "localhost:8090")
+@FeignClient(name = "point-api", url = "localhost:8090")
 public interface PointFeignClient
 {
 
