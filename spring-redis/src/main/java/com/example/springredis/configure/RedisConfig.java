4434d0a30265f4a89e2d6195ef2eca85c97bb258
hocaron <kkannu0407@gmail.com>
2024-09-23 12:45:18 +0900

    feat: add enableKeyspaceEvents

--- spring-redis/src/main/java/com/example/springredis/configure/RedisConfig.java
+++ spring-redis/src/main/java/com/example/springredis/configure/RedisConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
