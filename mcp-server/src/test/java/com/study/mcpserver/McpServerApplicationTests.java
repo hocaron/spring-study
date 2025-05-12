66c5be0ccfb3ba704451a731aab1f4318916a52b
hocaron <kkannu0407@gmail.com>
2025-05-12 20:00:23 +0900

    feat: create spring boot ai project

---
+++ mcp-server/src/test/java/com/study/mcpserver/McpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
